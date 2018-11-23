a51e953e7ad5dc80bd1d2ffe77a21ecc13f94552
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-23 12:59:43 +0300

    deleted

--- app/auth/views.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from . import main
-from flask import render_template,redirect, request, url_for,abort,flash
-from flask_login import login_required, current_user
-from ..models import User,
-from .. import db
-
-
-app = Flask(__name__)
