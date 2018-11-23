d901f4272f75302e40ac44bf1168629af29a5ede
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-23 16:23:18 +0300

    inputted the mail server and set the maill username and password to environ

--- config.py
+++ config.py
@@ -3,8 +3,16 @@ import os
 class Config():
     SECRET_KEY=os.environ.get('SECRET_KEY')
     
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:1205@localhost/blog'
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:1205@localhost/bloger'
+    
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'sylveromondibloger'
+    SENDER_EMAIL = 'sylveromondi@gmail.com'
+    
 class ProdConfig(Config):
     pass
 
