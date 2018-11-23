f926d18a1acb51a56b7e3ee422808905154a8051
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-23 12:32:53 +0300

    initialized database

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
 
 
-
+db = SQLAlchemy()
 def create_app(config_name):
     
     app = Flask(__name__)
 
     app.config.from_object(config_options[config_name])
+
+    #  intializing
+    db.init_app(app)
 
     # registering the main app blueprint
     from .main import main as main_blueprint
