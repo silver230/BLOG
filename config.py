41aae3705f9ccd97bbf3b409840026baa1f0b738
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-23 16:47:56 +0300

    set destination for photos

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config():
     SECRET_KEY=os.environ.get('SECRET_KEY')
     
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:1205@localhost/bloger'
-    
+    UPLOAD_PHOTOS_DEST = 'app/static/photos'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
