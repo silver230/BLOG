2b459561d335eaa5d8b487212d5ae9992ca52423
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-23 12:28:41 +0300

    set database uri

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import os
 
 class Config():
     SECRET_KEY=os.environ.get('SECRET_KEY')
+    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:1205@localhost/blog'
 
 class ProdConfig(Config):
     pass
