c9b6fbf4b1b8b7a2968f25d7d6f6c005b5ab2b61
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-24 11:29:15 +0300

    initializedw flask mail

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,8 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager 
-from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -12,8 +12,8 @@ login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
-photos = UploadSet('photos', IMAGES)
 mail = Mail()
+photos = UploadSet('photos', IMAGES)
 
 def create_app(config_name):
     
