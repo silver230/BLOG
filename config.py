09d0c5fc691414cfab005beb730d40f177efa881
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-24 11:28:47 +0300

    corrected syntax for runtime destination for photos

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 
 class Config():
     SECRET_KEY=os.environ.get('SECRET_KEY')
-    
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:1205@localhost/bloger'
-    UPLOAD_PHOTOS_DEST = 'app/static/photos'
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
@@ -12,7 +12,7 @@ class Config():
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     SUBJECT_PREFIX = 'sylveromondibloger'
     SENDER_EMAIL = 'sylveromondi@gmail.com'
-    
+    SQLALCHEMY_TRACK_MODIFICATIONS = 'True'
 class ProdConfig(Config):
     pass
 
